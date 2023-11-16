1cffbdaf76c9a2f2c3f7a8a641f61e540caea3c1
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-11-16 16:10:41 +0000

    equipment added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :equipment
   resources :weapons
   resources :units
   resources :models
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_16_160431) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_16_160549) do
+  create_table "equipment", force: :cascade do |t|
+    t.integer "model_id", null: false
+    t.integer "weapon_id", null: false
+    t.integer "limits"
+    t.integer "slot"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "factions", force: :cascade do |t|
     t.string "name"
     t.string "banner"
